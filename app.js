4b4acd46490443ce205e4edf79daddbb147938ee
Isha <ishajainpdx@gmail.com>
2021-01-15 16:42:58 -0500

    Finished creating the bot, works as intended

--- app.js
+++ app.js
@@ -33,37 +33,61 @@ testCld('Bonjour');
 const translate = require('@k3rn31p4nic/google-translate-api');
 const ISO6391 = require('iso-639-1');
 
-async function test(text, lang) {
+async function translateText(item, text, lang) {
     let result;
     try {
-        if (ISO6391.getCode(lang) == '') throw err;
-        result = await translate(text, {to: ISO6391.getCode(lang)});
+        let isocode = ISO6391.getCode(lang);
+        if (isocode == '') throw err;
+        if (isocode == 'zh') isocode = 'zh-cn';
+        result = await translate(text, {to: isocode});
     }
     catch (err) { //error not being catched
-        console.log("CANNOT DETECT LANGUAGE\nPLEASE MAKE SURE YOU SPELL THE LANGUAGE CORRECTLY\n\nIF THAT'S NOT THE ISSUE, I DO NOT SUPPORT THAT LANGUAGE\nSORRY :(");
+        item.reply("CANNOT DETECT LANGUAGE\n\nPLEASE MAKE SURE YOU SPELL THE LANGUAGE CORRECTLY\n\nIF THAT'S NOT THE ISSUE, I DO NOT SUPPORT THAT LANGUAGE\n\nSORRY :(");
         return;
     }
-    if (result.from.text.autoCorrected) console.log('Did you mean \'' + result.from.text.value + '\'?');
-    console.log('Detected language: ' + ISO6391.getName(result.from.language.iso));
-    console.log('Translating to ' + lang + ': ' + result.text);
+    let response = '';
+    if (result.from.text.autoCorrected) response = response.concat('Did you mean \'' + result.from.text.value + '\'?\n\n');
+    if (result.from.language.iso != 'zh-CN' && result.from.language.iso != 'zh-TW') response = response.concat('Detected language: ' + ISO6391.getName(result.from.language.iso) + '\n');
+    else response = response.concat('Detected language: Chinese\n')
+    response = response.concat('\nTranslating to ' + lang + ': ' + result.text);
+    console.log('RESPONSE: ' + response);
+    item.reply(response);
 }
-test('Hey there, my name is Isha', 'French');
+//translateText('Hello my freind, what is the date?', 'French');
 
 const BOT_START = Date.now() / 1000;
 
 const comments = new CommentStream(client, {
-    subreddit: 'all',
+    subreddit: 'testingground4bots',
     limit: 10,
-    pollTime: 10000
+    pollTime: 2000
 });
 
 const canSummon = (msg) => {
-    return msg && msg.toLowerCase().includes('/u/translate-text');
+    return msg && msg.toLowerCase().includes('translate-text');
 };
 
 comments.on('item', (item) => {
+    let language = 'English';
     if (item.created_utc < BOT_START) return;
     if (!canSummon(item.body)) return;
-    item.reply('hello world');
     console.log(item);
+    if (item.body.replace('translate-text', '').trim() != '') {
+        language = item.body.replace('translate-text', '').trim();
+    }
+    //item.reply('hello world');
+    let prefix = item.parent_id.substring(0,3);
+    if (prefix == 't3_') {
+        console.log('t3_');
+        client.getSubmission(item.parent_id).fetch().then(submission => {
+            console.log(submission.selftext);
+            translateText(item, submission.selftext, language);
+        });
+    } else {
+        console.log('t1_');
+        client.getComment(item.parent_id).fetch().then(parentComment => {
+            console.log(parentComment.body);
+            translateText(item, parentComment.body, language);
+        });
+    }
 });
